d0fd992b831448b746cc007c454239bfeed4321f
Miguel Mamani <miguel.coder.pe@gmail.com>
2024-08-26 01:42:30 -0500

    Se agrega componente de pasos y fragmentos para el formulario

---
+++ app/src/main/java/com/eas/app/PasoDatosPersonalesFragment.java
@@ -0,0 +1,29 @@
+package com.eas.app;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PasoDatosPersonalesFragment extends Fragment {
+
+    private EditText etNombres;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_paso_datos_personales, container, false);
+
+        etNombres = view.findViewById(R.id.nombres);
+
+        return view;
+    }
+}
--- app/src/main/java/com/eas/app/RegistroUsuarioActivity.java
+++ app/src/main/java/com/eas/app/RegistroUsuarioActivity.java
@@ -1,24 +1,78 @@
 package com.eas.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.eas.componentes.StepView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
+
+    private int currentStep = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registro_usuario);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        final StepView stepView = findViewById(R.id.step_view);
+
+        stepView.setOnStepClickListener(new StepView.OnStepClickListener() {
+            @Override
+            public void onStepClick(int step) {
+                Toast.makeText(RegistroUsuarioActivity.this, "Step " + step, Toast.LENGTH_SHORT).show();
+                loadFragment(currentStep);
+            }
         });
+
+        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentStep < stepView.getStepCount() - 1) {
+                    currentStep++;
+                    stepView.go(currentStep, true);
+                } else {
+                    stepView.done(true);
+                }
+            }
+        });
+
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentStep > 0) {
+                    currentStep--;
+                }
+                stepView.done(false);
+                stepView.go(currentStep, true);
+            }
+        });
+
+        List<String> steps = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            steps.add("Step " + (i + 1));
+        }
+        steps.set(steps.size() - 1, steps.get(steps.size() - 1) + " last one");
+        stepView.setSteps(steps);
+
+        loadFragment(currentStep);
+    }
+
+    private void loadFragment(int step) {
+        Fragment fragment = new PasoDatosPersonalesFragment();
+        FragmentManager firstFragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = firstFragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment, "pasoDatosPersonalesFragment");
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        System.out.println("Llego....");
     }
 }
