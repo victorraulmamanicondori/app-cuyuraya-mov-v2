ca61f560d82a269784dbf5505b5f28a20e782166
Victor Raul Mamani Condori <victorr.mamanic@gmail.com>
2024-09-10 01:45:53 -0500

    Se completa las tarjetas de la pantalla principal

--- app/src/main/java/com/eas/app/PrincipalActivity.java
+++ app/src/main/java/com/eas/app/PrincipalActivity.java
@@ -1,24 +1,48 @@
 package com.eas.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    private CardView cardRegistrarUsuario;
+    private CardView cardAsignarMedidor;
+    private CardView cardRegistrarLectura;
+    private CardView cardRegistrarEgreso;
+    private CardView cardRegistrarIngreso;
+    private CardView cardGestionLecturas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_principal);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        cardRegistrarUsuario = findViewById(R.id.cardRegistrarUsuario);
+        cardRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(PrincipalActivity.this, RegistroUsuarioActivity.class);
+                //startActivity(intent);
+                Toast.makeText(PrincipalActivity.this, "Card clicked", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
